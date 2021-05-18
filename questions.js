7537f7e7b6ea73699f38991df384eb3b2130adaa
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-18 12:38:14 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"people",
+        options:["you", "they", "we"],
+        answer: 1
+    },
+    {
+     q:"Tom",
+        options:["he", "she", "it"],
+        answer: 0   
+    },
+    {
+        q:"an orange",
+        options:["she", "he", "it"],
+        answer: 2
+    },
+    {
+        q:"animals",
+        options:[ "we", "you", "they"],
+        answer: 2
+    },
+    {
+        q:"Joy and Monica",
+        options:["they", "we", "he",],
+        answer: 0
+    },
+    {
+        q:"a boy",
+        options:["she", "he", "it"],
+        answer: 1
+    },
+    {
+        q:"a woman",
+        options:["she", "he", "it"],
+        answer: 0
+    },
+    {
+        q:"books",
+        options:["it", "they", "we"],
+        answer: 1
+    },
+    {
+        q:"a dog",
+        options:["she", "it", "he"],
+        answer: 1
+    },
+    {
+        q:"roses",
+        options:["it", "we", "they"],
+        answer: 2
+    }
+
+]
